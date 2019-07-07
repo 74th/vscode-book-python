b1e5c9a622b7c7db2f026b4ee59bbd45d5063b6d
74th <site@74th.tech>
2019-07-07 21:26:43 +0900

    seabornの導入

---
+++ data_science/README.md
@@ -0,0 +1,9 @@
+Using Python 3.7.2
+
+```
+# using MacOS mojave
+# https://qiita.com/zreactor/items/c3fd04417e0d61af0afe
+brew install python pyenv pyenv-virtualenv
+pyenv virtuenv -p /usr/local/python3.7 data_science
+pyenv local data_science
+```
---
+++ data_science/note.py
@@ -0,0 +1,16 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%%
+# iris データセットをの読み込み
+df = pd.read_csv("./iris.csv")
+df.head()
+
+#%%
+# Nameごとのヒストグラムの表示
+df.hist(by="Name", column="SepalLength", sharex=True, sharey=True)
+
+#%%
+sns.pairplot(df, hue='Name')
